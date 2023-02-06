c6b16a00c081bcad2e6e5660569917484ea2bd8e
mmmky <w.m124@icloud.com>
2023-02-06 17:39:46 +0900

    02/06SATD commitID_file

---
+++ src/main/java/main_time.java
@@ -0,0 +1,74 @@
+import org.eclipse.jgit.api.BlameCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.blame.BlameResult;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.jfree.ui.action.ActionRadioButton;
+import org.refactoringminer.api.GitService;
+import org.refactoringminer.util.GitServiceImpl;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class main_time {
+
+    public static List<String> file_name = new ArrayList();
+    public static List<String> miner_message = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        Path directory = Paths.get("C:\\Users\\wm124\\summer_inter\\satd1\\eclipse.jdt.core");//aspectj.eclipse.jdt.core
+        GitService gitService = new GitServiceImpl();
+        boolean reverse = false;
+        try {
+            FileInputStream fis_name = new FileInputStream("file_name.txt");
+            FileInputStream fis_miner = new FileInputStream("miner_message.txt");
+            BufferedReader bf_name = new BufferedReader(new InputStreamReader(fis_name, "UTF-8"));
+            BufferedReader bf_miner = new BufferedReader(new InputStreamReader(fis_miner, "UTF-8"));
+            String content;
+            while ((content = bf_name.readLine()) != null) {
+                file_name.add(content);
+                System.out.println(content);
+            }
+            bf_name.close();
+            while ((content = bf_miner.readLine()) != null) {
+                miner_message.add(content);
+                System.out.println(content);
+            }
+            bf_miner.close();
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            Repository repository = gitService.openRepository("eclipse.jdt.core");
+            Git git = new Git(repository);
+            ObjectId startCommit = repository.resolve(file_name.get(0));//ここを開始拠点とする
+            BlameCommand blamer = git.blame();
+            if (!reverse) {//普通のBlame
+                blamer.setStartCommit(startCommit);
+            } else {//リバースの場合
+                blamer.reverse(startCommit, repository.resolve("HEAD"));
+            }
+            blamer.setFilePath(file_name.get(0));
+            BlameResult result = blamer.call();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
--- src/main/java/main_yuta.java
+++ src/main/java/main_yuta.java
@@ -1,4 +1,3 @@
-import com.sun.xml.internal.bind.v2.TODO;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -6,70 +5,131 @@ import org.refactoringminer.api.GitService;
 import org.refactoringminer.util.GitServiceImpl;
 
 import satd_detector.core.utils.SATDDetector;
+import weka.core.pmml.jaxbbindings.False;
+import weka.core.pmml.jaxbbindings.True;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+//日付import
+
+//グラフ作成ライブラリ
+//matplotっでしようかな？？
 public class main_yuta {
-    public static int true_number = 0;
-    public static int false_number = 0;
+    public static int added_number = 0;
+    public static int deleted_number = 0;
+
+    public static String file_check = null;
     public static List<String> true_message = new ArrayList<>();
+    public static List<String> miner_message = new ArrayList<>();
 
-    public static List<String> all_true = new ArrayList<>();
+    public static List<String> file_name = new ArrayList<>();
 
-    public static List<String> false_message = new ArrayList<>();
+    public static List<String> coment = new ArrayList<>();
+
+    public static boolean miner_check = false;
+
+
     public static void main(String[] args) throws IOException, InterruptedException {
-        Path directory = Paths.get("C:\\Users\\wm124\\summer_inter\\satd1\\aspectj.eclipse.jdt.core");
+        Path directory = Paths.get("C:\\Users\\wm124\\summer_inter\\satd1\\eclipse.jdt.core");//aspectj.eclipse.jdt.core
         GitService gitService = new GitServiceImpl();
+        File file_add = new File("C:\\Users\\wm124\\summer_inter\\satd1\\ture_message.txt");
+        File file_miner = new File("C:\\Users\\wm124\\summer_inter\\satd1\\miner_message.txt");
+        File file_path = new File("C:\\Users\\wm124\\summer_inter\\satd1\\file_name.txt");
+        File file_commit = new File("C:\\Users\\wm124\\summer_inter\\satd1\\commitID.txt");
+        FileWriter filewriter_add = new FileWriter(file_add);
+        FileWriter filewriter_miner = new FileWriter(file_miner);
+        FileWriter filewriter_name = new FileWriter(file_path);
+        FileWriter filewriter_commit = new FileWriter(file_commit);
+
+        int addedSATDnumber[] = new int[15];
+        int deletdSATDnumber[] = new int[15];
+        int commitNumber[] = new int[15];
+
+        //TODO jgitdd
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        List<String> start = Arrays.asList("2016-06-22", "2017-06-28", "2018-06-27", "2018-09-19", "2018-12-19", "2019-03-20", "2019-06-19", "2019-09-19", "2019-12-18", "2020-03-18", "2020-06-17", "2020-09-16", "2020-12-16", "2021-03-17", "2021-06-16");//, "2020-06-16", "2020-06-16", "2021-09-15", "2021-12-08", "2022-03-16"
+//        List<String> end = Arrays.asList("2017-06-27", "2018-06-27", "2018-09-19", "2018-12-19", "2019-03-20", "2019-06-19", "2019-09-19", "2019-12-18", "2020-03-18", "2020-06-17", "2020-09-16", "2020-12-16", "2020-03-17", "2020-06-16");//, "2020-06-16", "2021-09-15", "2021-12-08", "2022-03-16", "2022-06-16"
 
 
-        //TODO jgit
-        try{
-            Repository repository = gitService.openRepository("aspectj.eclipse.jdt.core");
-            Git git = new Git(repository);
-            Iterable<RevCommit> log = git.log().call();
 
-            for(RevCommit commit:log){
-                LocalDateTime now = LocalDateTime.ofInstant(commit.getAuthorIdent().getWhen().toInstant(), ZoneId.systemDefault());
-                String date = String.valueOf(now);
-                if(date.contains("2016")){
-                    System.out.println("時刻"+now);
-                    runCommand(directory, "git", "diff", "--no-ext-diff","--unified=0","--no-prefix","-a", "-w", commit.getName()+"^.."+commit.getName());
+            try {
+                Repository repository = gitService.openRepository("eclipse.jdt.core");
+                Git git = new Git(repository);
 
+                Iterable<RevCommit> log = git.log().call();
 
-                }
+                for (RevCommit commit : log) {
+                    LocalDateTime commitDate = LocalDateTime.ofInstant(commit.getAuthorIdent().getWhen().toInstant(), ZoneId.systemDefault());
+                    commitDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-                try{
-                    File file = new File("C:\\Users\\wm124\\summer_inter\\satd1\\ture_message.txt");
-                    FileWriter filewriter = new FileWriter(file);
-                    for ( int i = 0; i<true_message.size();  i++){
-                        filewriter.write(true_message.get(i)+"\n");
+                    for (int i = 0; i < start.size()-1; i++) {
+                        int number = 0;
+                        LocalDate date_start = LocalDate.parse(start.get(i), formatter);
+                        LocalDate date_end = LocalDate.parse(start.get(i+1), formatter);
+
+                        LocalDateTime releaseDate_start = date_start.atStartOfDay();
+                        LocalDateTime releaseDate_end = date_end.atStartOfDay();
+                        //System.out.println(commit);
+                        if (commitDate.isBefore(releaseDate_start) ) {
+                            break;
+                        }else if (commitDate.isBefore(releaseDate_end)){
+                            added_number = 0;
+                            deleted_number = 0;
+                            runCommand(directory, "git", "diff", "--no-ext-diff", "--unified=0", "--no-prefix", "-a", "-w", commit.getName() + "^.." + commit.getName());
+                            addedSATDnumber[i]+=added_number;
+                            deletdSATDnumber[i]+=deleted_number;
+                            commitNumber[i]++;
+                            if(miner_check == true) {
+                                filewriter_commit.write(commit.getName() + "\n");
+                            }
+                            miner_check = false;
+                        }//&& releaseDate_end.isAfter(commitDate)) || releaseDate_start.isEqual(commitDate) || releaseDate_end.isEqual(commitDate
+                            number++;
+                            //commitDate.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+                            //System.out.println("時刻" + commitDate);
                     }
 
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
                 }
 
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            for (String s : true_message) {
+                filewriter_add.write(s + "\n");
+            }
+            for (String s : miner_message) {
+                filewriter_miner.write(s + "\n");
+            }
+            for (String s:file_name){
+                filewriter_name.write(s +"\n");
             }
 
+            //Tnumber[i] = true_number;
+            //Mnumber[i] = miner_number;
+            //Gitnumber[i]= number;
+            //System.out.println(number);
 
-
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-
-
+        //System.out.println("true回数" + added_number + "miner回数" + deleted_number);
+        System.out.println("Tnumber"+ Arrays.toString(addedSATDnumber));
+        System.out.println("Mnumber"+ Arrays.toString(deletdSATDnumber));
+        System.out.println("Number" + Arrays.toString(commitNumber));
     }
+    //グフラ作成
+
     public static void show(Process process) throws IOException, InterruptedException {
         StringBuilder output = new StringBuilder();
 
@@ -127,40 +187,80 @@ public class main_yuta {
         @Override
         public void run() {
             StringBuilder output = new StringBuilder();
-            StringBuilder miner= new StringBuilder();
+            StringBuilder miner = new StringBuilder();
             SATDDetector detector1 = new SATDDetector();
 
 
             try (BufferedReader br = new BufferedReader(new InputStreamReader(is));) {
                 String line;
                 while ((line = br.readLine()) != null) {
-                        String regex = "\\s*\\+\\s*.*\\/\\/(.*)";  //  "\\s*-\\s*.*\\/\\/(.*)"
-                        Pattern p = Pattern.compile(regex);
-                        System.out.println(line);
-                        Matcher m = p.matcher(line);
-                        if(m.find()) {
-                            String check = m.group(1);
-                            boolean result = detector1.isSATD(check);
-                            if (result) {
-                                true_number += 1;
-                                true_message.add(String.valueOf(true_number)+":"+m.group(1));
-                            } else {
-                                false_number += 1;
-                                false_message.add(m.group(1));
-                            }
-                            System.out.println("結果" + result+"回数"+true_number+" "+true_message);
-                            sleep(0);
+                    String regex_add = "\\s*\\+\\s*.*\\/\\/(.+)";
+                    String regex_miner = "\\s*-\\s*.*\\/\\/(.+)";
+                    String file_match = "^diff\\s-{2}git\\s(.{1,})";
+                    boolean add = false;
+                    boolean mina = false;
 
+                    //
+                    Pattern p_add = Pattern.compile(regex_add);
+                    Pattern p_miner = Pattern.compile(regex_miner);
+                    Pattern p_file = Pattern.compile(file_match);
+
+                    //System.out.println(line);
+
+                    Matcher m_add = p_add.matcher(line);
+                    Matcher m_miner = p_miner.matcher(line);
+                    Matcher m_file = p_file.matcher(line);
+                    //System.out.println(line);
+
+                    if(m_file.find()){
+                        file_check = m_file.group(1);
+                        //System.out.println(file_check);
+                    }
+
+                    if (m_add.find()) {
+                        String check = m_add.group(1);
+                        boolean result = detector1.isSATD(check);
+
+                        if (result) {
+                            added_number += 1;
+                            true_message.add(String.valueOf(added_number) + ":" + m_add.group(0));
+                            add = true;
                         }
+
+                    }
+
+                    if (m_miner.find()) {
+                        String check = m_miner.group(1);
+
+                        boolean result = detector1.isSATD(check);
+                        if (result) {
+                            miner_check = true;
+                            deleted_number += 1;
+                            miner_message.add(String.valueOf(deleted_number) + ":" + m_miner.group(0));
+                            mina = true;
+                        }
+
+                    }
+
+                    if(mina == true ){
+                        file_name.add(file_check);
+                        //System.out.println("2"+file_name);
+                    }
+
+
+
+
+
                 }
-                System.out.println("true回数"+true_number+"false回数"+false_number);
-                System.out.println("true_message"+true_message);
 
 
-            } catch (IOException | InterruptedException ioe) {
-                ioe.printStackTrace();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
 
+
 }
+
+
